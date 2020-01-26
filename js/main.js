85d38e504191d0bb7e8fc40cd87f9d324cc75c2a
Nikhil Bishnoi <nikhilbish@gmail.com>
2020-01-26 16:24:09 +0530

    yo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+    
+    if (window.pageYOffset > sticky)
+    {
+    navbar.classList.add("sticky")
+    } 
+    else {
+    navbar.classList.remove("sticky");
+    }
+}
+
