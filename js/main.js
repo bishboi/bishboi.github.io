e2aa951a72fac55b2b2ee6b36b73ee21f51e0362
Nikhil Bishnoi <nikhilbish@gmail.com>
2020-02-21 16:24:01 +0530

    changed header

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ $(window).on('load',function() {
     $(".spinner-wrapper").fadeOut("slow");;
 });
 
+/*scroll down-------------------------------------*/
+$(function() {
+	$('a[href*="#"]').on("click", function(e) {
+		e.preventDefault();
+		$("html, body").animate(
+			{ scrollTop: $($(this).attr("href")).offset().top },500,"linear");
+	});
+});
 
 
 /*
@@ -403,7 +411,6 @@ setInterval(() => {
 FOOOOOOOOTERRRRRRRRRRRRRRR CLOSED
 ------------------------------------------------------------------------------------------------------------------
 */
-
 
 
 
