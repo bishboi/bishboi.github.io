886d60253be1f99018ff7621d153c9504840f28c
Nikhil Bishnoi <nikhilbish@gmail.com>
2020-02-21 15:02:03 +0530

    header changed

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 
-window.onscroll = function() {myFunction()};
 
-function myFunction() {
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
-    
-    if (window.pageYOffset > sticky)
-    {
-    navbar.classList.add("sticky")
-    } 
-    else {
-    navbar.classList.remove("sticky");
-    }
-}
 $(window).on('load',function() {
     // Animate loader off screen
     $(".spinner-wrapper").fadeOut("slow");;
